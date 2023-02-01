659fa8a7101396b253ff69049a124a3fd903b0dc
gp-1108 <pietrogirotto00@gmail.com>
2023-02-01 21:16:08 +0100

    last check added

--- booking.js
+++ booking.js
@@ -1,4 +1,5 @@
 const { SlotNotAvailableException } = require('./exceptions.js');
+const { NoUserException } = require('./exceptions.js');
 
 module.exports = async function(page, time) {
   const slots = (await page.$x("//div[@class='row align-items-center']/div[@class='col-6']/div[@class='h3 mb-1']/parent::div/parent::div"));
@@ -17,7 +18,12 @@ module.exports = async function(page, time) {
   const btn = (await slots[i].$x(".//button"))[0];
   await btn.click();
   await new Promise(resolve => setTimeout(resolve, 2000));
-  const firstCheckbox = (await page.$x("//div[@class='custom-control custom-checkbox']"))[0];
+  let firstCheckbox = null;
+  try {
+    firstCheckbox = (await page.$x("//div[@class='custom-control custom-checkbox']"))[0];
+  } catch (err) {
+    throw new NoUserException("No profile already created");
+  }
   const proceedBtn = (await page.$x("//button[text()='Continua']"))[0];
   await firstCheckbox.click();
   await proceedBtn.click();
